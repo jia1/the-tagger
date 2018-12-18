00311cf093f858cb71d23464b12d4f7ee3a18523
Jiayee <jiayee.lim@visenze.com>
2018-12-18 15:54:02 +0800

    Test things out with Telethon

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
 from flask import Flask
+from telethon import TelegramClient, events, sync
+import os
+
 app = Flask(__name__)
+api_id = os.environ['API_ID']
+api_hash = os.environ['API_HASH']
+group = os.environ['GROUP']
 
 @app.route('/')
 def index():
     return 'Hello, World!'
+
+@app.route('/pingu')
+def pingu():
+    client = TelegramClient('session_name', api_id, api_hash)
+    with client:
+        usernames = client.get_participants(group)
+        print(usernames)
 
